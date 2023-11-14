fc213052275c269270ec5438b67cc4e67530e661
ksimaster <ksimasterlord@gmail.com>
2023-11-14 21:12:34 +0300

    test AD on home

---
+++ Assets/Scripts/ADScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using InstantGamesBridge;
+using InstantGamesBridge.Modules.Social;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ADScript : MonoBehaviour
+{
+
+   
+
+}
