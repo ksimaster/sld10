971f177b37e2301cbac126e1ff7b548dc73ec708
ksimaster <ksimasterlord@gmail.com>
2023-11-24 21:32:52 +0300

    fix on/off build

--- Assets/Scripts/VKScript.cs
+++ Assets/Scripts/VKScript.cs
@@ -3,26 +3,137 @@ using System.Collections.Generic;
 using UnityEngine;
 using InstantGamesBridge;
 using InstantGamesBridge.Modules.Advertisement;
+using InstantGamesBridge.Common;
 
 public class VKScript : MonoBehaviour
 {
+    private InterstitialState _state;
+    public GameObject gameplay;
     private void Start()
     {
         
+        
+        
+    }
+
+    private void Update()
+    {
+
+
     }
 
     public void Inter()
     {
+        Bridge.advertisement.interstitialStateChanged += OnInterstitialStateChanged;
         Bridge.advertisement.ShowInterstitial();
+        
     }
 
     public void Reward()
     {
+        Bridge.advertisement.rewardedStateChanged += RewardedResult;
         Bridge.advertisement.ShowRewarded();
     }
     public void Share()
     {
         Bridge.social.InviteFriends();
     }
+    
+    private void OnInterstitialStateChanged(InterstitialState stateInter)
+    {
+        switch (stateInter)
+        {
+            case InterstitialState.Opened:
+                AudioListener.pause = true;
+                break;
+            case InterstitialState.Failed:
+                AudioListener.pause = false;
+                break;
+            case InterstitialState.Closed:
+                AudioListener.pause = false;
+                break;
+            case InterstitialState.Loading:
+                AudioListener.pause = false;
+                break;
 
+        }
+        /*
+        if (stateInter.Equals("Opened"))
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            Bridge.advertisement.interstitialStateChanged -= OnInterstitialStateChanged;
+        }
+        */
+    }
+
+   private void RewardedResult(RewardedState state)
+    {
+        switch (state)
+        {
+            case RewardedState.Opened:
+                AudioListener.pause = true;
+                break;
+            case RewardedState.Failed:
+                AudioListener.pause = false;
+                break;
+            case RewardedState.Closed:
+                AudioListener.pause = false;
+                break;
+            case RewardedState.Loading:
+                AudioListener.pause = false;
+                break;
+            case RewardedState.Rewarded:
+                if (PlayerPrefs.GetInt("Rewarded") == 1)
+                {
+                    gameplay.GetComponent<Gameplay>().Hint();
+                    //Bridge.advertisement.rewardedStateChanged -= RewardedResult;
+                }
+                if (PlayerPrefs.GetInt("Rewarded") == 2)
+                {
+                    gameplay.GetComponent<Gameplay>().Next();
+                    //Bridge.advertisement.rewardedStateChanged -= RewardedResult;
+                }
+                break;
+        }
+
+        /*
+        if (state.Equals("Opened") || state.Equals("Rewarded"))
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            
+        }
+        //Состояние Rewarded, выдача награды 1 и 2
+        if (state.Equals("Rewarded"))
+        {
+            if (PlayerPrefs.GetInt("Rewarded") == 1)
+            {
+                gameplay.GetComponent<Gameplay>().Hint();
+                Bridge.advertisement.rewardedStateChanged -= RewardedResult;
+            }
+            if (PlayerPrefs.GetInt("Rewarded") == 2)
+            {
+                gameplay.GetComponent<Gameplay>().Next();
+                Bridge.advertisement.rewardedStateChanged -= RewardedResult;
+            }
+        }
+        */
+    }
+
+    public void RewardHint()
+    {
+        PlayerPrefs.SetInt("Rewarded", 1);
+    }
+
+    public void RewardNext()
+    {
+        PlayerPrefs.SetInt("Rewarded", 2);
+    }
 }
